bdfe68127dd8aa3b4a2159b34070700b28d1920c
sharonyh <sharonyh@umich.edu>
2020-04-21 12:12:21 +0800

    Week 10

--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/Entities/Coin.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/Entities/Coin.java
@@ -1,11 +1,18 @@
 package au.edu.unsw.infs3634.cryptobag.Entities;
 
 import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+@Entity
+
 public class Coin {
+
+    @PrimaryKey
+    @NonNull
 
     @SerializedName("id")
     @Expose
