bf58aebfeceeaf3c393c369ea5e616cfa9d0b34a
sharonyh <sharonyh@umich.edu>
2020-04-21 12:11:53 +0800

    Week 10

--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/DetailFragment.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/DetailFragment.java
@@ -12,12 +12,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.room.Room;
 
 import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.List;
 
 import au.edu.unsw.infs3634.cryptobag.Entities.Coin;
+import au.edu.unsw.infs3634.cryptobag.CoinDao;
 import au.edu.unsw.infs3634.cryptobag.Entities.CoinLoreResponse;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,6 +32,7 @@ import static android.content.ContentValues.TAG;
 public class DetailFragment extends Fragment {
     public static final String ARG_ITEM_ID = "item_id";
     private Coin mCoin;
+    private CoinDatabase mDb;
 
     public DetailFragment() {}
 
@@ -37,51 +40,60 @@ public class DetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mDb = Room.databaseBuilder(getContext(), CoinDatabase.class, "coin-database").build();
+        Log.d(TAG, "Line 30");
+
         if(getArguments().containsKey(ARG_ITEM_ID)) {
-            new GetCoinTask().execute();
+//            new GetCoinTask().execute();
+            new GetCoinDBTask().execute(getArguments().getString(ARG_ITEM_ID));
         }
     }
 
-    private class GetCoinTask extends AsyncTask<Void, Void, List<Coin>> {
+    private class GetCoinDBTask extends AsyncTask<String, Void, Coin> {
         @Override
-        protected List<Coin> doInBackground(Void... voids){
-            try{
-                Log.d(TAG, "doInBackground: SUCCESS");
-
-                //create Retrofit instance & parse the retrieved JSON using GSON deserializer
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://api.coinlore.com")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-
-                //get service & call object for the request
-                CoinService service = retrofit.create(CoinService.class);
-                Call<CoinLoreResponse> coinsCall = service.getCoins();
-
-                Response<CoinLoreResponse> coinResponse = coinsCall.execute();
-                List<Coin> coins = coinResponse.body().getData();
-                return coins;
-            }
-
-            catch(IOException e){
-                Log.d(TAG, "onFailure: FAILURE");
-                e.printStackTrace();
-                return null;
-            }
+        protected Coin doInBackground(String...ids) {
+            return mDb.coinDao().getCoin(ids[0]);
         }
 
         @Override
-        protected void onPostExecute(List<Coin> coins){
-            for(Coin coin : coins) {
-                if(coin.getId().equals(getArguments().getString(ARG_ITEM_ID))) {
-                    mCoin = coin;
-                    updateUi();
-                    DetailFragment.this.getActivity().setTitle(mCoin.getName());
-                    break;
-                }
-            }
+        protected void onPostExecute(Coin coin){
+            mCoin = coin;
+            updateUi();
         }
     }
+
+
+
+//    private class GetCoinTask extends AsyncTask<Void, Void, List<Coin>> {
+//        @Override
+//        protected List<Coin> doInBackground(Void... voids){
+//            try{
+//                Log.d(TAG, "doInBackground: SUCCESS");
+//
+//                //create Retrofit instance & parse the retrieved JSON using GSON deserializer
+//                Retrofit retrofit = new Retrofit.Builder()
+//                        .baseUrl("https://api.coinlore.com")
+//                        .addConverterFactory(GsonConverterFactory.create())
+//                        .build();
+//
+//                //get service & call object for the request
+//                CoinService service = retrofit.create(CoinService.class);
+//                Call<CoinLoreResponse> coinsCall = service.getCoins();
+//
+//                Response<CoinLoreResponse> coinResponse = coinsCall.execute();
+//                List<Coin> coins = coinResponse.body().getData();
+//                return coins;
+//            }
+//
+//            catch(IOException e){
+//                Log.d(TAG, "onFailure: FAILURE");
+//                e.printStackTrace();
+//                return null;
+//            }
+//        }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
